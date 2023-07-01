6a74bc3d9d594f97b3543d10b03b07c21d5e33a5
Yusuf <ososkovy@gmail.com>
2023-06-30 22:03:50 -0400

    Func Polling return string message, added struct of message

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"CurrencyNotifications/Structs"
 	"CurrencyNotifications/Tokens"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
-func main() {
-	resp, err := http.Get(fmt.Sprintf("https://api.telegram.org/bot%s/getUpdates", Tokens.TgToken))
+func Polling(TgToken string) string {
+	resp, err := http.Get(fmt.Sprintf("https://api.telegram.org/bot%s/getUpdates", TgToken))
 	if err != nil {
 		log.Fatal(err)
 	}
 	body, err := io.ReadAll(resp.Body)
-	fmt.Println(string(body))
+	var req Structs.Req
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return req.Result[0].Message.Text
+}
+
+func main() {
+	fmt.Println(Polling(Tokens.TgToken))
 }
